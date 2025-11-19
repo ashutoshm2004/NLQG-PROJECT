b52f768d32f9546eb39bed2f7dee7315f68cd387
ashutoshm2004 <ashutm59@gmail.com>
2025-11-19 15:03:10 +0530

    requirements and cors fixed

--- backend/app.py
+++ backend/app.py
@@ -14,10 +14,16 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://127.0.0.1:5173", "*"],
-    allow_credentials = True,
-    allow_headers=["*"],
+    allow_origins=[
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+        "https://nl2sqlg.netlify.app",
+        "https://*.netlify.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
+    allow_headers=["*"],
+    expose_headers=["Content-Disposition"],
 )
 
 from utils.db_manager import DATABASE_PATH
