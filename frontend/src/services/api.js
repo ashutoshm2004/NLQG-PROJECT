9eadd403a629a89e4e442c4261c80655fe4192ea
ashutoshm2004 <ashutm59@gmail.com>
2025-11-19 13:39:05 +0530

    RENDER

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE = "http://127.0.0.1:8000";
+const BASE = import.meta.env.VITE_API_BASE_URL;
 
 export const uploadFile = async (file) => {
   const form = new FormData();
